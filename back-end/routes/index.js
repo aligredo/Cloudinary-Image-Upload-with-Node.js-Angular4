aba5946d47849860830d705cd2602317fe595afe
aligredo <aliahmedismail37@gmail.com>
2018-05-02 20:38:07 +0200

    Documented code

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,25 +1,27 @@
 var express = require("express"),
-	router = express.Router();
-	var cloudinary = require('cloudinary');
-	var multer  = require('multer');
-	var cloudinaryStorage = require('multer-storage-cloudinary');
-	var path = require('path');
-	var express = require('express');
-	var app = express();
+	router = express.Router(),
+	 cloudinary = require('cloudinary'),
+	 multer  = require('multer'),
+	 cloudinaryStorage = require('multer-storage-cloudinary'),
+	 path = require('path'),
+	 express = require('express'),
+	 app = express();
 
+	//Configuring Cloduinary API
 	cloudinary.config({  //Your Cloudinary API Data
 	  cloud_name: '*********',
 	  api_key: '*************',
 	  api_secret: '*************************'
 	});
 
+//The post request to call the multer uploader
 router.post("/sendImage",
 multer({storage: cloudinaryStorage({
  cloudinary: cloudinary,
  allowedFormats: ['jpg', 'png'],
  destination: function (req, file, callback) { callback(null, './uploads');},
  filename: function (req, file, callback) { callback(null, "MyImage")}}) //MyImage is the name of the image which will be uploaded to your Cloudinary storage
-}).single('Image'), function(req, res){
+}).single('Image'), function(req, res){ //To return OK status to the user after uploading
 	return res.status(200).json({
 		msg:"Uploaded"
 	})
--- front-end/cloudinary-image-management/src/app/app.component.ts
+++ front-end/cloudinary-image-management/src/app/app.component.ts
@@ -19,27 +19,29 @@ export class AppComponent {
   ) {
   }
 
-   onFileChange(event) {
+   onFileChange(event) { //Method to set the value of the file to the selected file by the user
      this.Image = event.target.files[0]; //To get the image selected by the user
      this.valid = true;
   }
 
-   onSubmit(event) {
-     var image = new FormData();
-     image.append('Image', this.Image);
-     this.appService
+   onSubmit(event) { //Method to send the request to the server
+     var image = new FormData(); //FormData creation
+     image.append('Image', this.Image); //Adding the image to the form data to be sent
+     this.appService //Sending the rquest from the service function
        .sendImage(image)
        .subscribe((res: any) => {
          console.log(res);
       });
+
     this.loading = true;
     setTimeout(() => {
       this.loading = false;
     }, 1000);
-    this.message = "Uploaded";
+
+    this.message = "Uploaded"; //Message to be viewed in App page
   }
 
-   clearFile() {
+   clearFile() { // Method to clear the selected file
      this.valid = false;
      this.message = "Cleared";
     this.fileInput.nativeElement.value = "";
